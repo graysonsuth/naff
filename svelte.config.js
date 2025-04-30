672058bc60440155455d33dd904d8d2faa33dd2b
Grayson Sutherland <me@Graysons-MacBook-Pro.local>
2025-04-30 18:20:36 +1200

    update

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
-  }
+    adapter: adapter({
+      // Cloudflare Pages configuration
+      routes: {
+        include: ['/*'],
+        exclude: []
+      }
+    })
+  },
+  preprocess: vitePreprocess()
 };
 
 export default config;
